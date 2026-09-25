efbf902741d5eae7f2a4855f2f0b618687bb57f9
Ricklima991 <Ricklima991@users.noreply.github.com>
2026-09-25 02:37:55 -0300

    Reconstrucao profissional com fidelidade a referencia (tokens, estrutura e fotos originais)

--- js/index.js
+++ js/index.js
@@ -1,13 +1,32 @@
-var image = window.document.getElementById("image-svg");
+/* Alterna as fotos do projeto ao clicar (image1 → image2 → image3) */
+(function () {
+  var photos = [
+    "./assets/img/image1.jpg",
+    "./assets/img/image2.jpg",
+    "./assets/img/image3.jpg"
+  ];
+  var current = 0;
+  var photo = document.getElementById("photo");
+  var dots = document.querySelectorAll(".photo-dots .dot");
 
-function toggleImage() {
-  if (image.getAttribute("xlink:href").includes("./assets/img/image1.jpg")) {
-    image.setAttribute("xlink:href", "./assets/img/image2.jpg");
-  } else if (
-    image.getAttribute("xlink:href").includes("./assets/img/image2.jpg")
-  ) {
-    image.setAttribute("xlink:href", "./assets/img/image3.jpg");
-  } else {
-    image.setAttribute("xlink:href", "./assets/img/image1.jpg");
+  function show(index) {
+    current = (index + photos.length) % photos.length;
+    photo.setAttribute("src", photos[current]);
+    dots.forEach(function (dot, i) {
+      dot.classList.toggle("active", i === current);
+    });
   }
-}
+
+  photo.addEventListener("click", function () {
+    show(current + 1);
+  });
+
+  photo.addEventListener("keydown", function (event) {
+    if (event.key === "Enter" || event.key === " ") {
+      event.preventDefault();
+      show(current + 1);
+    }
+  });
+
+  photo.setAttribute("tabindex", "0");
+})();
